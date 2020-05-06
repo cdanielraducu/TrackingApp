93703c93669b6c0c587ba1c0f6c9a80416af6493
Alexandru-Cristian Teniță <tenitaalex@yahoo.com>
2020-05-06 21:48:37 +0300

    User cu tot

--- companyV2/companyV2/src/main/java/com/example/companyV2/model/User.java
+++ companyV2/companyV2/src/main/java/com/example/companyV2/model/User.java
@@ -4,7 +4,11 @@ public class User {
 
     private String firstName;
     private String lastName;
-    private int age;
+    private String cnp;
+    private String email;
+    private String pass;
+    private String dataAng;
+
 
     public String getFirstName() {
         return firstName;
@@ -22,11 +26,47 @@ public class User {
         this.lastName = lastName;
     }
 
-    public int getAge() {
-        return age;
+    public String getCnp() {
+        return cnp;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setCnp(String cnp) {
+        this.cnp = cnp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getDataAng() {
+        return dataAng;
+    }
+
+    public void setDataAng(String data_ang) {
+        this.dataAng = dataAng;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", cnp='" + cnp + '\'' +
+                ", email='" + email + '\'' +
+                ", pass='" + pass + '\'' +
+                ", data_ang='" + dataAng + '\'' +
+                '}';
     }
 }
