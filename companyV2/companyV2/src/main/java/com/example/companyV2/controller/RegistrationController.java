83fe5a259ff3fa69fe162a9c63d679cda2eea4a2
Alexandru-Cristian Teniță <tenitaalex@yahoo.com>
2020-05-11 20:37:51 +0300

    MERGE DIN BODY CU POSTMAN!!!!!

--- companyV2/companyV2/src/main/java/com/example/companyV2/controller/RegistrationController.java
+++ companyV2/companyV2/src/main/java/com/example/companyV2/controller/RegistrationController.java
@@ -2,15 +2,22 @@ package com.example.companyV2.controller;
 
 import com.example.companyV2.model.Company;
 import com.example.companyV2.model.Registration;
+import com.example.companyV2.model.User;
+import com.example.companyV2.repositories.RegistrationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
+
+    @Autowired
+    RegistrationRepository registrationRepository;
 
     @GetMapping("registration")
     public String getRegistration(@ModelAttribute("registration") Company company) {
@@ -20,10 +27,18 @@ public class RegistrationController {
     }
 
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute("registration") Company company) {
+    public String addRegistration(@RequestBody Map<String, String> body) {
+        System.out.println(body);
+        String CUI = body.get("cui");
+        String name = body.get("name");
+        String email = body.get("email");
+        String password = body.get("password");
 
-        System.out.println("Registration: " + company.getName() + " avand CUI " + company.getCUI() + " cu adresa de mail " + company.getEmail()
-        + " cu parola " + company.getPassword());
+        System.out.print(CUI + " " + name + " "+email+ " "+ password);
+
+        registrationRepository.save(new Company(CUI,name,email, password));
+        //System.out.println("Registration: " + company.getName() + " avand CUI " + company.getCUI() + " cu adresa de mail " + company.getEmail()
+        //+ " cu parola " + company.getPassword());
 
         return "redirect:registration";
     }
