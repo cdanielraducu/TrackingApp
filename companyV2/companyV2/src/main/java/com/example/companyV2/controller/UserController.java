e1bd02afc9493181647e6da1ec7d91c1856b8804
Alexandru-Cristian Teniță <43493834+teniii@users.noreply.github.com>
2020-05-12 15:59:44 +0300

    merge. acum si userii.

--- companyV2/companyV2/src/main/java/com/example/companyV2/controller/UserController.java
+++ companyV2/companyV2/src/main/java/com/example/companyV2/controller/UserController.java
@@ -3,34 +3,44 @@ package com.example.companyV2.controller;
 import com.example.companyV2.repositories.UserRepository;
 import com.example.companyV2.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/user")
+    @GetMapping("/user/all")
     public List<User> index() {
         return userRepository.findAll();
     }
 
+    @GetMapping("/user")
+    public String getUser(@ModelAttribute("user") User user) {
+
+        return "user";
+    }
+
     @PostMapping("/user")
-    public User create(@RequestBody Map<String, String> body){
-        String cnp = body.get("cnp");
+    public String addRegistration(@RequestParam String cnp,
+                                  @RequestParam String firstname,
+                                  @RequestParam String lastname,
+                                  @RequestParam String email,
+                                  @RequestParam String password){
+
+        /*String cnp = body.get("cnp");
         String firstname = body.get("firstname");
         String lastname = body.get("lastname");
         String email = body.get("email");
-        String password = body.get("password");
+        String password = body.get("password");*/
 
-        return userRepository.save(new User(cnp,firstname, lastname, email, password));
+        userRepository.save(new User(cnp,firstname, lastname, email, password));
+        return  "redirect:user";
     }
 
 }
