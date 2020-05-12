d9ce323ffb0bc33e0ccb89473e5a11c186d5a120
cdanielraducu <c.daniel_raducu@yahoo.com>
2020-05-12 16:43:26 +0300

    Dani

--- companyV2/companyV2/src/main/java/com/example/companyV2/controller/GreetingsController.java
+++ companyV2/companyV2/src/main/java/com/example/companyV2/controller/GreetingsController.java
@@ -8,15 +8,15 @@ import java.util.Map;
 @Controller
 public class GreetingsController {
 
-    @GetMapping("greeting")
-    public String greeting (Map<String, String> model) {
-
-        return "greeting";
-    }
-
-    @GetMapping("thyme")
-    public String thyme (Map<String, String> model) {
-        model.put("message", "Hello Thymeleaf");
-        return "thyme";
-    }
+//    @GetMapping("registration")
+//    public String greeting (Map<String, String> model) {
+//
+//        return "registration";
+//    }
+//
+//    @GetMapping("thyme")
+//    public String thyme (Map<String, String> model) {
+//        model.put("message", "Hello Thymeleaf");
+//        return "thyme";
+//    }
 }
